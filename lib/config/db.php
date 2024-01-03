0b8518de95a2eccf28cb1d1c500a597b44e5641c
ctrhub <crafterhub.info@gmail.com>
2024-01-03 23:23:24 +0200

    DB setup

--- lib/config/db.php
+++ lib/config/db.php
@@ -4,25 +4,62 @@ return array(
   'webkit_template' => array(
     'id'                => array('int', 11, 'null' => 0, 'autoincrement' => 1),
     'name'              => array('varchar', 255, 'null' => 0),
+    'cover_url'         => array('text', 'null' => 0),
 
-    // ---- Front ---- //
+    'front_content'     => array('mediumtext', 'null' => 1),
+    'front_styles'      => array('mediumtext', 'null' => 1),
+    'front_scripts'     => array('mediumtext', 'null' => 1),
+    'front_fonts'       => array('mediumtext', 'null' => 1),
 
-    'front_content'           => array('mediumtext', 'null' => 1),
-    'front_styles'            => array('mediumtext', 'null' => 1),
-    'front_scripts'           => array('mediumtext', 'null' => 1),
-    'front_fonts'             => array('mediumtext', 'null' => 1),
-
-    // ---- Editor ---- //
-
-    'components'      => array('longtext', 'null' => 1),
-    'styles'          => array('mediumtext', 'null' => 1),
-    'assets'          => array('mediumtext', 'null' => 1),
-    'fonts'           => array('mediumtext', 'null' => 1),
+    'editor_components' => array('longtext', 'null' => 1),
+    'editor_styles'     => array('mediumtext', 'null' => 1),
+    'editor_assets'     => array('mediumtext', 'null' => 1),
+    'editor_fonts'      => array('mediumtext', 'null' => 1),
 
     'create_datetime'   => array('datetime', 'null' => 0),
     'update_datetime'   => array('datetime', 'null' => 0),
     ':keys'             => array(
       'PRIMARY' => 'id',
+    ),
+  ),
+
+  'webkit_project' => array(
+    'id'                => array('int', 11, 'null' => 0, 'autoincrement' => 1),
+    'name'              => array('varchar', 255, 'null' => 0),
+    'image_url'         => array('text', 'null' => 0),
+    'preview_image_url' => array('text', 'null' => 0),
+
+    'create_datetime'   => array('datetime', 'null' => 0),
+    'update_datetime'   => array('datetime', 'null' => 0),
+    ':keys'             => array(
+      'PRIMARY' => 'id',
+    ),
+  ),
+
+  'webkit_template_project' => array(
+    'id'                => array('int', 11, 'null' => 0, 'autoincrement' => 1),
+    'template_id'       => array('int', 11, 'unsigned' => 1, 'null' => 0),
+    'project_id'        => array('int', 11, 'unsigned' => 1, 'null' => 0),
+    'status'            => array('int', 11, 'unsigned' => 1, 'null' => 0),
+
+    ':keys'             => array(
+      'PRIMARY' => 'id',
+      'template_id' => 'template_id',
+    ),
+  ),
+
+  'webkit_template_condition' => array(
+    'id'                => array('int', 11, 'null' => 0, 'autoincrement' => 1),
+    'template_id'       => array('int', 11, 'unsigned' => 1, 'null' => 0),
+    'app_id'            => array('varchar', 255, 'null' => 0),
+    'theme_id'          => array('varchar', 255, 'null' => 0),
+    'template_type'     => array('varchar', 255, 'null' => 0),
+
+    ':keys'             => array(
+      'PRIMARY' => 'id',
+      'template_id' => 'template_id',
+      'app_id' => 'app_id',
+      'theme_id' => 'theme_id',
     ),
   ),
 );
